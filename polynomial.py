2a12a6cb5840610c0d86572c7119cad63e5025cf
jjaassoonn <jujian.zhang1998@out.com>
2021-11-24 17:24:48 +0000

    polynomial multiplication

--- polynomial.py
+++ polynomial.py
@@ -1,6 +1,16 @@
+from __future__ import annotations
+
 class polynomial(object):
-    def __init__(self,deg,coeff):
-        self.deg = deg
-        
-        
+    def __init__(self, coeff : dict):
+        self.coeff = coeff
+        self.deg = max(coeff.keys())
+
+    def __repr__(self):
+        return " + ".join([f"{v}*x^{k}" for k,v in self.coeff.items()])
+
+    def __mul__(self, g : polynomial):
+        new_coeff = dict()
+        for i in range(self.deg + g.deg + 1):
+            new_coeff[i] = sum(self.coeff.get(j, 0) * g.coeff.get(i - j, 0) for j in range(i))
+        return polynomial(new_coeff)
         
